a4930e0986c49f187b6309577b63f7d18d615a3d
Matthew Gross <MatthewGross@users.noreply.github.com>
2014-08-23 21:00:10 -0400

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,8 @@
+/*
+ * Author: Matthew Gross
+ * Website: http://mattgross.net/
+ * Description of File: This file will be loaded if an ad-blocked is disabled or not-active. As such, the file will also...
+ * ... be denied if an adblocker is enabled, allowing webmasters to perform certain functions depending on an adblocker status.
+*/
+
+var CanRunAds = true;
